bd15a8d3f46f734ae56b625ad993a269b5f72466
AronT <aron.thompson@racunarstvo.hr>
2021-02-12 01:05:15 +0100

    Fixed runtime editor

--- Assets/Source/RuntimeLevelEditor.cs
+++ Assets/Source/RuntimeLevelEditor.cs
@@ -5,87 +5,147 @@ using System.IO;
 
 public class RuntimeLevelEditor : MonoBehaviour
 {
-	int selectedX = -1;
-	int selectedY = -1;
-	Level level;
-	// Start is called before the first frame update
-	void Start()
+    int selectedX = -1;
+    int selectedY = -1;
+
+    int currentLevel = 1;
+
+    Level level;
+    // Start is called before the first frame update
+    void Start()
 	{
 		init();
 	}
 
-	public void init()
-	{
-		if (File.Exists(Util.getLevelPath(1)))
-		{
-			level = Util.deserialize(Util.getLevelPath(1));
-		}
-		else
-		{
-			level = new Level();
-			level.grid = new TileType[5][];
-			level.data = new TileData[5][];
-			for (int i = 0; i < level.grid.Length; i++)
-			{
-				level.grid[i] = new TileType[5];
-				level.data[i] = new TileData[5];
-				for (int j = 0; j < level.data[i].Length; j++)
-				{
-					level.data[i][j] = new TileData();
-				}
-			}
-		}
-	}
+    public void init(int levelNum = 1)
+    {
+        LoadLevel(levelNum);
+    }
+
+    public void LoadLevel(int lvlNumber)
+    {
+        if (File.Exists(Util.getLevelPath(lvlNumber)))
+        {
+            level = Util.deserialize(Util.getLevelPath(lvlNumber));
+        }
+        else
+        {
+            level = new Level();
+            level.grid = new TileType[5][];
+            level.data = new TileData[5][];
+            level.goalType = 1;
+            for (int i = 0; i < level.grid.Length; i++)
+            {
+                level.grid[i] = new TileType[5];
+                level.data[i] = new TileData[5];
+                for (int j = 0; j < level.data[i].Length; j++)
+                {
+                    level.data[i][j] = new TileData();
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        LoadLevellFields();
+        GUI.color = Color.white;
+        // SAVE
+        var saveY = level.grid[0].Length * 50;
+        if (GUI.Button(new Rect(0, saveY, 100, 50), "Save"))
+        {
+            Util.serialize(level, Util.getLevelPath(currentLevel));
+           // AssetDatabase.Refresh();
+        }
+
+        // LOAD
+        if (GUI.Button(new Rect(0, saveY + 50, 100, 50), "Load lvl 1"))
+        {
+            currentLevel = 1;
+            LoadLevel(1);
+            LoadLevellFields();
+           // AssetDatabase.Refresh();
+        }
+        if (GUI.Button(new Rect(100, saveY + 50, 100, 50), "Load lvl 2"))
+        {
+            currentLevel = 2;
+            LoadLevel(2);
+            LoadLevellFields();
+          //  AssetDatabase.Refresh();
+        }
+        if (GUI.Button(new Rect(200, saveY + 50, 100, 50), "Load lvl 3"))
+        {
+            currentLevel = 3;
+            LoadLevel(3);
+            LoadLevellFields();
+          //  AssetDatabase.Refresh();
+        }
+        GUI.Label(new Rect(300, saveY + 50, 100, 50), "Loaded lvl " + currentLevel);
 
 
-	void OnGUI()
-	{
-		if (level == null || level.grid == null)
-		{
-			init();
-		}
+        GUI.BeginGroup(new Rect(0, level.grid[0].Length * 50 + 150, Screen.width, Screen.height));
+        GUILayout.BeginVertical();
+        GUILayout.Label("");
+        GUILayout.Label("Goal: ", GUILayout.Width(70));
+        level.goalType = (int)GUILayout.HorizontalSlider(level.goalType, 1, 3, GUILayout.Width(50));
+        //GUILayout.Label("", GUILayout.Width(70));
+        switch (level.goalType)
+        {
+            case 1:
+                GUILayout.Label("Kill all enemies", GUILayout.Height(40));
+                break;
+            case 2:
+                GUILayout.Label("Kill the boss", GUILayout.Height(40));
+                break;
+            case 3:
+                GUILayout.Label("Reach goal in 10 steps", GUILayout.Height(40));
+                break;
+            default:
+                break;
+        }
+        GUILayout.EndVertical();
+        GUI.EndGroup();
 
-		for (int i = 0; i < level.grid.Length; i++)
-		{
-			for (int j = 0; j < level.grid[i].Length; j++)
-			{
-				GUI.color = Util.getColor(level.grid[i][j]);
-				if (GUI.Button(new Rect(i * 100, j * 50, 100, 50), level.grid[i][j].ToString()))
-				{
-					if (Event.current.button == 1)
-					{
-						selectedX = i;
-						selectedY = j;
-					}
-					else
-					{
-						var value = (int)(level.grid[i][j] + 1) % 9;
-						level.grid[i][j] = (TileType)value;
-					}
-				}
-			}
-		}
-		GUI.color = Color.white;
+        if (selectedX != -1 && selectedY != -1)
+        {
+            var tile = level.data[selectedX][selectedY];
 
-		var saveY = level.grid[0].Length * 50;
-		if (GUI.Button(new Rect(0, saveY, 100, 50), "Save"))
-		{
-			Util.serialize(level, Util.getLevelPath(1));
-			// AssetDatabase.Refresh();
-		}
+            GUI.BeginGroup(new Rect(0, level.grid[0].Length * 50 + 50, Screen.width, Screen.height));
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("isRanged: ", GUILayout.Width(70));
+            tile.isRanged = GUILayout.Toggle(tile.isRanged, "");
+            GUILayout.EndHorizontal();
 
-		if (selectedX != -1 && selectedY != -1)
-		{
-			var tile = level.data[selectedX][selectedY];
+            GUI.EndGroup();
+        }
+    }
 
-			GUI.BeginGroup(new Rect(0, level.grid[0].Length * 50 + 50, Screen.width, Screen.height));
-                GUILayout.BeginHorizontal();
-                    GUILayout.Label("isRanged: ", GUILayout.Width(70));
-                    tile.isRanged = GUILayout.Toggle(tile.isRanged, "");
-					GUILayout.Label("isRanged: ", GUILayout.Width(70));
-                    tile.isRanged = GUILayout.Toggle(tile.isRanged, "");
-                GUILayout.EndHorizontal();
-			GUI.EndGroup();
-		}
-	}
+    private void LoadLevellFields()
+    {
+        if (level == null || level.grid == null)
+        {
+            init();
+        }
+
+        for (int i = 0; i < level.grid.Length; i++)
+        {
+            for (int j = 0; j < level.grid[i].Length; j++)
+            {
+                GUI.color = Util.getColor(level.grid[i][j]);
+                if (GUI.Button(new Rect(i * 100, j * 50, 100, 50), level.grid[i][j].ToString()))
+                {
+                    if (Event.current.button == 1)
+                    {
+                        selectedX = i;
+                        selectedY = j;
+                    }
+                    else
+                    {
+                        var value = (int)(level.grid[i][j] + 1) % 9;
+                        level.grid[i][j] = (TileType)value;
+                    }
+                }
+            }
+        }
+    }
 }
